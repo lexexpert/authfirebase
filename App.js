d67ba3652c4fb50b01fe5452a2f9b54cc326406c
lexexpert <ivanreshetar@gmail.com>
2019-06-03 14:16:35 -0500

    Firebase code replaced

--- App.js
+++ App.js
@@ -8,22 +8,15 @@
 
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import firebase from 'firebase';
 import {Header} from './src/components/common';
 import LoginForm from './src/components/LoginForm';
+import Firebase from './src/apis/Firebase';
+
 
 export default class App extends Component {
 
   componentWillMount() {
-    firebase.initializeApp({
-      apiKey: "AIzaSyBLZrgwIeVHV-M4ZJjDveDg0L_Oe-lHFeE",
-      authDomain: "authentication-5fd5d.firebaseapp.com",
-      databaseURL: "https://authentication-5fd5d.firebaseio.com",
-      projectId: "authentication-5fd5d",
-      storageBucket: "authentication-5fd5d.appspot.com",
-      messagingSenderId: "867518603426",
-      appId: "1:867518603426:web:bebbb10276aa5cfa"
-    })
+    Firebase();
   }
 
 
